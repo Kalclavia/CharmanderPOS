67b525ee15f6ce5cc78a833ce5f5b8c422370db2
Adeeb Ismail <adeebi5412@protonmail.com>
2024-10-25 10:48:06 -0500

    Started login page

---
+++ client/src/Login.vue
@@ -0,0 +1,18 @@
+<script setup>
+import './assets/login.css'
+</script>
+<template>
+  <body>
+    <div class="flex-container-login">
+      <h1>ID:</h1>
+      <input placeholder="Enter Your ID" />
+    </div>
+  </body>
+</template>
+
+<!-- <style scoped>
+header,
+main {
+  padding: 20px;
+}
+</style> -->
