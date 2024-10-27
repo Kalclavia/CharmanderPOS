9202ad490c2dd09d913ca70998030911c3f7ce17
JensynH <jensynnn@gmail.com>
2024-10-27 14:23:39 -0500

    Fixed UI of menu bar.

--- client/src/components/MenuBar.vue
+++ client/src/components/MenuBar.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="menu-bar">
-        <button @click="$emit('selectItem', 'Appetizers')">Appetizers</button>
-        <button @click="$emit('selectItem', 'Bowl')">Bowl</button>
-        <button @click="$emit('selectItem', 'Plate')">Plate</button>
-        <button @click="$emit('selectItem', 'Bigger Plate')">Bigger Plate</button>
-        <button @click="$emit('selectItem', 'Drinks')">Drinks</button>
-        <button @click="$emit('selectItem', 'A La Carte')">A La Carte</button>
+        <h2 class="menu-title">MENU</h2>
+        <button @click="$emit('selectItem', 'Appetizers')">APPETIZERS</button>
+        <button @click="$emit('selectItem', 'Bowl')">BOWL</button>
+        <button @click="$emit('selectItem', 'Plate')">PLATE</button>
+        <button @click="$emit('selectItem', 'Bigger Plate')">BIGGER PLATE</button>
+        <button @click="$emit('selectItem', 'Drinks')">DRINKS</button>
+        <button @click="$emit('selectItem', 'A La Carte')">A LA CARTE</button>
     </div>
 </template>
 
@@ -15,4 +16,42 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.menu-bar {
+    background-color: #e7e4d7;
+    width: 450px;
+    height: 100vh;
+    padding: 20px;
+    position: fixed;
+    display: flex;
+    flex-direction: column; /* Align items vertically */
+    box-shadow: 0 4px 8px #080808;
+    top: 0;
+    left: 0;
+    z-index: 1000;
+}
+
+.menu-title {
+    margin-bottom: 15px; /* Space between title and buttons */
+    font-size: 24px;
+    text-align: center;
+    color: #080808;
+}
+
+button {
+    border: 2px solid #080808;
+    border-radius: 30px;
+    background-color: #e7e4d7;
+    color: #080808;
+    font: Arial;
+    padding: 15px;
+    margin-bottom: 10px;
+    cursor: pointer; /* Change cursor on hover */
+    transition: background-color 0.3s, box-shadow 0.3s; /* Transition effects */
+    box-shadow: 0 4px 3px #080808;
+}
+
+button:hover {
+    background-color: #d2ceb8; /* Darker background on hover */
+}
+</style>
