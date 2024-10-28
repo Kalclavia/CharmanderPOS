db0c9eb40a6645101da4f2c87d8b82ecc7681d10
Adeeb Ismail <adeebi5412@protonmail.com>
2024-10-28 12:28:18 -0500

    Added some login logic

--- client/src/Login.vue
+++ client/src/Login.vue
@@ -1,18 +1,50 @@
-<script setup>
-import './assets/login.css'
-</script>
+<!-- <script setup> -->
+<!-- </script> -->
+<!-- v-model: holds the variable, v-on: what to do when enter key is pressed -->
+
 <template>
   <body>
     <div class="flex-container-login">
       <h1>ID:</h1>
-      <input placeholder="Enter Your ID" />
+      <input
+        placeholder="Enter Your ID"
+        v-model="inputID"
+        v-on:keyup.enter="onEnter"
+      />
     </div>
   </body>
 </template>
 
-<!-- <style scoped>
-header,
-main {
-  padding: 20px;
+<script>
+import './assets/login.css'
+import axios from 'axios'
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      employeeIds: [],
+      inputID: '',
+    }
+  },
+  methods: {
+    fetchEmployees() {
+      axios
+        .get('http://localhost:3000/employees')
+        .then(res => {
+          this.employeeIds = res.data.map(employee => employee.employeeid)
+        })
+        .catch(error => console.log(error))
+    },
+    onEnter() {
+      for (var i = 0; i < this.employeeIds.length; i++) {
+        if (this.inputID == this.employeeIds[i]) {
+        }
+      }
+    },
+  },
+  mounted() {
+    this.fetchEmployees() // Fetch menu items when the component mounts
+  },
 }
-</style> -->
+</script>
