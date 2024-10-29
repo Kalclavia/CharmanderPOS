1ccb3a447e347a0ffe5b479c988471f4e4f28b53
Kalclavia <abhi.bhattacharyya@tamu.edu>
2024-10-29 15:36:26 -0500

    Added files from login branch

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import SelfCheckout from '../views/self-checkout.vue'
 import Menu1 from '../views/menu1.vue'
 import Menu2 from '../views/menu2.vue'
+import LoginPage from '../views/Login.vue'
 
 const routes = [
   { path: '/', name: 'SCO', component: SelfCheckout },
   { path: '/menu1', name: 'Menu Board 1', component: Menu1 },
   { path: '/menu2', name: 'Menu Board 2', component: Menu2 },
+  { path: '/login', name: 'Login', component: LoginPage },
 ]
 
 const router = createRouter({
