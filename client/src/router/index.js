d88dda44c311d1d981ebc30b95585d4a2f17ac92
Kalclavia <abhi.bhattacharyya@tamu.edu>
2024-10-29 14:35:39 -0500

    Moved code from App.vue into new file, views/self-checkout.vue, and updated App.vue & router/index.js to fix routing

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,25 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import LaunchPage from '../components/LaunchPage.vue';
-import HomePage from '../components/HomePage.vue';
-import Cart from '../components/Cart.vue';
-import CheckoutPage from '../components/CheckoutPage.vue';
-import OrderComplete from '../components/OrderComplete.vue';
-import Appetizers from '../components/menu/Appetizers.vue';
-import Bowl from '../components/menu/Bowl.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import SelfCheckout from '../views/self-checkout.vue'
 
-const routes = [
-  { path: '/', component: LaunchPage },
-  { path: '/home', component: HomePage },
-  { path: '/cart', component: Cart },
-  { path: '/checkout', component: CheckoutPage },
-  { path: '/order-complete', component: OrderComplete },
-  { path: '/appetizers', component: Appetizers },
-  { path: '/bowl', component: Bowl },
-];
+const routes = [{ path: '/', name: 'SCO', component: SelfCheckout }]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
+})
 
-export default router;
+export default router
