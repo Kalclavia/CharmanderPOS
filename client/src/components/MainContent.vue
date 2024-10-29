3fd9820d23dd402a6ab9dfce8a251fa4d72801e8
Sana <109184461+sanamemon04@users.noreply.github.com>
2024-10-29 13:04:13 -0500

    implemented cart button and panel

--- client/src/components/MainContent.vue
+++ client/src/components/MainContent.vue
@@ -7,6 +7,19 @@
         <component v-if="item === 'Bigger Plate'" :is="'BiggerPlate'" @selectItem="selectItem" />
         <component v-if="item === 'Drinks'" :is="'Drink'" @selectItem="selectItem" />
         <component v-if="item === 'A La Carte'" :is="'ALaCarte'" @selectItem="selectItem" />
+        <!-- <button class="toggle-button" @click="togglePanel">Toggle Panel</button> -->
+       <!-- Image inside a button -->
+       <button class="cart-button" @click="toggleCart">
+            <img 
+                src="../assets/cart.png" 
+                alt="Cart Panel" 
+                class="cart-image"
+            />
+        </button>
+        <div v-show="isCartVisible" class="panel">
+            <h3>Shopping Cart</h3>
+            <!-- Add your panel content here -->
+        </div>
     </div>
 </template>
 
@@ -23,10 +36,18 @@ export default {
     props: {
         item: String,
     },
+    data() {
+        return {
+            isCartVisible: false
+        }
+    },
     methods: {
         selectItem(item) {
             // Handle the selected item here
             console.log('Selected item:', item);
+        },
+        toggleCart() {
+            this.isCartVisible = !this.isCartVisible;
         }
     },
     components: {
@@ -51,5 +72,43 @@ export default {
     color: #e7e4d7;
     padding: 40px;
     overflow-y: auto;
+}
+
+.cart-button {
+    position: fixed;
+    top: 30px; /* Adjust as needed */
+    right: 45px; /* Adjust as needed */
+    background-color: #e7e4d7; 
+    border: none; /* Remove border */
+    cursor: pointer; /* Change cursor on hover */
+    transition: background-color 0.3s, box-shadow 0.3s; /* Transition effects */
+    box-shadow: 0 4px 3px #080808;
+    border-radius: 75px;
+    z-index: 1001;
+    
+}
+
+.cart-button:hover {
+    background-color: #d2ceb8;
+}
+
+.cart-image {
+    width: 50px; /* Adjust size */
+    height: auto; /* Maintain aspect ratio */
+}
+
+
+.panel {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 40%;
+    height: 100%;
+    background-color: #000000e3;
+    color: #e7e4d7;
+    box-shadow: -2px 0 5px rgba(0,0,0,0.1);
+    padding: 20px;
+    overflow-y: auto;
+    z-index: 999;
 }
 </style>
