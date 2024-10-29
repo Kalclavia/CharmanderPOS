e31e85ddff326b3f232775eb7da52dd5998f6e0b
Kalclavia <abhi.bhattacharyya@tamu.edu>
2024-10-29 15:12:25 -0500

    Added menu boards at routes /menu1 and /menu2

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SelfCheckout from '../views/self-checkout.vue'
+import Menu1 from '../views/menu1.vue'
+import Menu2 from '../views/menu2.vue'
 
-const routes = [{ path: '/', name: 'SCO', component: SelfCheckout }]
+const routes = [
+  { path: '/', name: 'SCO', component: SelfCheckout },
+  { path: '/menu1', name: 'Menu Board 1', component: Menu1 },
+  { path: '/menu2', name: 'Menu Board 2', component: Menu2 },
+]
 
 const router = createRouter({
   history: createWebHistory(),
