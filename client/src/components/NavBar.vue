c06990c9c836d446a73116ecde4d100fe43faf8f
Kalclavia <abhi.bhattacharyya@tamu.edu>
2024-10-29 21:19:47 -0500

    Manually pulled in changes from manager-view branch

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="menu-bar" :style="{ width: sideBarWidth }">
+      <span class="collapsebar" @click="toggleSideBar">
+        <span class="arrow">&gt;&gt;</span>
+      </span>
+        <span v-if="collapsed">
+
+        </span>
+        <span v-else>
+            <h2 class="title">OPTIONS</h2>
+            <button style="width: 200px;" @click="$emit('selectItem', 'Home')">Home Page</button>
+            <button style="width: 200px;" @click="$emit('selectItem', 'Inventory')">Menu Items</button>
+            <button style="width: 200px;" @click="$emit('selectItem', 'Menu Prices')">Menu Prices</button>
+            <button style="width: 200px;" @click="$emit('selectItem', 'Manage Employees')">Manage Employees</button>
+            <button style="width: 200px;" @click="$emit('selectItem', 'View Reports')">Reports</button>
+
+        </span>
+    </div>
+  </template>
+  
+  <script>
+  import { collapsed, toggleSideBar, sideBarWidth } from './state.js';
+  
+  export default {
+    name: 'MenuBar',
+    setup() {
+      return { collapsed, toggleSideBar, sideBarWidth };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .menu-bar {
+    background-color: #e7e4d7;
+    width: 250px;
+    height: 100vh;
+    padding: 20px;
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0 4px 8px #080808;
+    top: 0;
+    left: 0;
+    z-index: 1000;
+    transition: width 0.3s ease;
+  }
+  
+  .title {
+    margin-bottom: 15px;
+    font-size: 24px;
+    text-align: center;
+    color: #080808;
+  }
+  
+   button {
+    border: 2px solid #080808;
+    border-radius: 30px;
+    background-color: #e7e4d7;
+    color: #080808;
+    font: Arial;
+    padding: 15px;
+    margin-bottom: 10px;
+    cursor: pointer;
+    transition: background-color 0.3s, box-shadow 0.3s;
+    box-shadow: 0 4px 3px #080808;
+  }
+  
+  button:hover {
+    background-color: #d2ceb8;
+  }
+  
+  .arrow {
+    font-size: 18px;
+    color: #080808;
+  }
+  </style>
+  
---
+++ client/src/views/manager.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <MenuBar @selectItem="selectItem" />
+    <MainContent :item="selectedItem" />
+  </div>
+</template>
+
+<script>
+import LaunchPage from '../components/ManagerHome.vue';
+import MenuBar from '../components/NavBar.vue';
+import MainContent from '../components/ManagerContent.vue';
+import { collapsed } from '../components/state.js';
+
+export default {
+  name: 'App',
+  components: {
+    LaunchPage,
+    MenuBar,
+    MainContent,
+  },
+  data() {
+    return {
+      isOnLaunchPage: true,
+      selectedItem: "Home", 
+    };
+  },
+  created() {
+    // Set initial states
+    this.isOnLaunchPage = true;
+    collapsed.value = true; 
+  },
+  methods: {
+    selectItem(item) {
+      this.selectedItem = item;
+      this.isOnLaunchPage = false;
+    },
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
