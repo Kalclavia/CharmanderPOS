cf6ced0025fc28a2db1746d1d59d837523d84a32
Kalclavia <abhi.bhattacharyya@tamu.edu>
2024-10-29 20:55:41 -0500

    Changed structure to be compatible with routed app + pages as views setup

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,25 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import LaunchPage from '../components/ManagerHome.vue';
-import HomePage from '../components/HomePage.vue';
-import Cart from '../components/Cart.vue';
-import CheckoutPage from '../components/CheckoutPage.vue';
-import OrderComplete from '../components/OrderComplete.vue';
-import Appetizers from '../components/menu/Appetizers.vue';
-import Bowl from '../components/menu/Bowl.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import manager from '../views/manager.vue'
 
-const routes = [
-  { path: '/', component: LaunchPage },
-  { path: '/home', component: HomePage },
-  { path: '/cart', component: Cart },
-  { path: '/checkout', component: CheckoutPage },
-  { path: '/order-complete', component: OrderComplete },
-  { path: '/appetizers', component: Appetizers },
-  { path: '/bowl', component: Bowl },
-];
+const routes = [{ path: '/manager', name: 'manager', component: manager }]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
+})
 
-export default router;
+export default router
