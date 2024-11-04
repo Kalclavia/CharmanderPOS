b8cef942be478dea6568b1e15bd98e6a88d460b6
vadit <59973483+vadit@users.noreply.github.com>
2024-11-04 13:48:03 -0600

    changes to index.js
    sort employees by id

--- server/src/index.js
+++ server/src/index.js
@@ -57,7 +57,7 @@ app.get("/python", (req, res) => {
  */
 app.get("/employees", (req, res) => {
   pool
-    .query("SELECT * FROM Employees;")
+    .query("SELECT * FROM Employees ORDER BY employeeid ASC;")
     .then((query_res) => res.json(query_res.rows))
     .catch((error) => res.status(500).json({ error: error.message }));
 });
