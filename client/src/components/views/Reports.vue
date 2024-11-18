97ba2a61cd2167988e0c4bf38db876d3ae0879e1
vadit <59973483+vadit@users.noreply.github.com>
2024-11-18 14:06:04 -0600

    added sales report (WIP)

--- client/src/components/views/Reports.vue
+++ client/src/components/views/Reports.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="buttons">
-    <button>Sales Report</button>
-    <button>Product Usage Report</button>
+    <button  @click="this.productBool = false, this.salesBool =true" >Sales Report</button>
+    <button @click="this.productBool = true, this.salesBool =false" >Product Usage Report</button>
     <button>X-Report</button>
     <button>Z-Report</button>
   </div>
-  <div style="padding-top: 20px">
+  <div v-if="productBool" style="padding-top: 20px">
     <productBarChartbarchart />
+  </div>
+  <div v-if="salesBool" style="padding-top: 20px">
+    <salesBarChart />
   </div>
   <div class="buttons"><button>Toggle Chart Style</button></div>
 </template>
@@ -14,15 +17,23 @@
 <script>
 import axios from 'axios'
 import productBarChartbarchart from '../charts/productBarChart.vue'
+import salesBarChart from '../charts/salesBarChart.vue';
 export default {
   name: 'Reports',
   components: {
     productBarChartbarchart,
+    salesBarChart
   },
   data() {
-    return { employeeTransaction: [], ingredientsByDateRange: [] }
+    return {
+      productBool : false,
+      salesBool : false,
+      employeeTransaction: [], 
+      ingredientsByDateRange: [],
+      salesByDateRange: [],
+    }
   },
-  methods: {
+  methods: {  
     fetchEmployeeTransaction() {
       const config = {
         headers: {
@@ -63,9 +74,29 @@ export default {
           console.error('Error fetching employee transaction:', error)
         })
     },
+    fetchSalesByDateRange(){
+      const config = {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        params: { startDate: '2023-01-01', endDate: '2023-01-02' },
+      }
+      axios
+        .get(
+          import.meta.env.VITE_API_ENDPOINT + 'report/itemsByDateRange',
+          config,
+        )
+        .then(response => {
+          this.salesByDateRange = response.data
+        })
+        .then(() => console.log(this.salesByDateRange))
+        .catch(error => {
+          console.error('Error fetching sales:', error)
+        })
+    },
   },
   mounted() {
-    //this.fetchIngredientsByDateRange()
+    this.fetchSalesByDateRange()
   },
 }
 </script>
