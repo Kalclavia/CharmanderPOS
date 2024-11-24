1d70e2729262bb1615802bae2de524df50f1cbc7
Kalclavia <abhi.bhattacharyya@tamu.edu>
2024-11-23 18:08:44 -0600

    Uncommented Vue3GoogleLogin include

--- client/src/main.js
+++ client/src/main.js
@@ -4,15 +4,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueCookies from 'vue-cookies'
-// import vue3GoogleLogin from 'vue3-google-login'
+import vue3GoogleLogin from 'vue3-google-login'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(VueCookies)
 
-// app.use(vue3GoogleLogin, {
-//     clientId: '964792734838-8vo14im7viclrevna15i5k7mid2n1v32.apps.googleusercontent.com'
-//   })
+app.use(vue3GoogleLogin, {
+    clientId: '964792734838-8vo14im7viclrevna15i5k7mid2n1v32.apps.googleusercontent.com'
+  })
 
 app.mount('#app')
